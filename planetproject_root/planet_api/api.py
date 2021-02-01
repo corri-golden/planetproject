682239bbbba43d156cb1e64cf780e8db1d98d451
Corri Golden <corri.golden@gmail.com>
2021-02-01 17:35:04 -0600

    endpoints for films and all films

--- planetproject_root/planet_api/api.py
+++ planetproject_root/planet_api/api.py
@@ -3,8 +3,21 @@ import requests
 
 BASE_URL = "https://swapi.dev/api/"
 
+
 def get_planet_by_id(id):
     url = f'{BASE_URL}planets/{id}/'
+    response = requests.get(url)
+
+    return response.status_code == 200, response.json()
+
+def get_films():
+    url = f'{BASE_URL}films'
+    response = requests.get(url)
+
+    return response.status_code == 200, response.json()
+
+def get_films_by_id(id):
+    url = f'{BASE_URL}films/{id}/'
     response = requests.get(url)
 
     return response.status_code == 200, response.json()
